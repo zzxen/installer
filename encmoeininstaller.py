c40143d2fcc82951361b663a598dc95a9f40674e
proxykingdev2 <117310322+proxykingdev2@users.noreply.github.com>
2022-11-05 12:27:27 +0330

    Update encmoeininstaller.py

--- encmoeininstaller.py
+++ encmoeininstaller.py
@@ -11,7 +11,7 @@ print("I WILL CHANGE VMESS PANEL FOR ENCRYPT.../")
 def install():
     panel_and_encryption = "https://github.com/proxykingdev2/x-ui-with-moein-encryption-protocol.git"
     command1 = "apt update -y && apt full-upgrade && git clone {}".format(panel_and_encryption)
-    command2 = "chmod +x mencx && ./mencx"
+    command2 = "chmod +x x-ui-with-moein-encryption-protocol/mencx && ./x-ui-with-moein-encryption-protocol/mencx"
 
     os.system(command1)
     time.sleep(2)
